c7bfc1cc4767a977d723927f985afd9c0afadbe7
santiracca <santiagoracca7@gmail.com>
2021-04-12 18:07:19 -0400

    Initial Commit

---
+++ Treker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Treker
+//
+//  Created by Santiago Racca on 4/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Treker/TrekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekerApp.swift
+//  Treker
+//
+//  Created by Santiago Racca on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
